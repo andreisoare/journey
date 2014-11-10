c83b82d6b9b22331f65058ffdf30b8aa90b3d851
Andrei Soare <andrei.soare@gmail.com>
2014-11-09 16:34:23 -0800

    fix

--- web/config/environment.js
+++ web/config/environment.js
@@ -45,9 +45,9 @@ module.exports = function(environment) {
 
   ENV.contentSecurityPolicy = {
     'default-src': "'none'",
-    'script-src': "'self' localhost:35729",
+    'script-src': "'self'",
     'font-src': "'self'",
-    'connect-src': "'self' localhost:35729",
+    'connect-src': "'self'",
     'img-src': "'self'",
     'style-src': "'self'",
     'media-src': "'self'"
