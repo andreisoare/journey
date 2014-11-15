8faeea5fca537d2b53fa9512d703c3d24a8b7f61
Andrei Soare <andrei.soare@gmail.com>
2014-11-14 17:41:10 -0800

    redirect from login to users if authenticated

--- web/app/routes/login.js
+++ web/app/routes/login.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  redirect: function() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('login');
+    }
+  }
 });
